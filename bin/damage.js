c5b1adbc0e8fa27d925d9a6c60eff69201357e14
伽塔 <106463935+Nilou-s-cat@users.noreply.github.com>
2022-12-10 17:04:01 +0800

    先简单实现了一下扩散，但是还差一个副反应逻辑没写好

--- bin/damage.js
+++ bin/damage.js
@@ -8,7 +8,16 @@ export function SetDmgConfig(avatarconfig){
         var avatarjson=JSON.parse(readFileSync(readpath))
     }
 }
-export function Zhengfa(avatarconfig,avatarpos,basedmg){
+function kuosan(avatarconfig,mainavatarpos,basedmg){
+    var extradmg = 1
+    switch(mainavatarpos){
+        case 1:
+            var avatarpos = mainavatarpos-1
+            var extraAttackPos = [1,2]
+            
+    }
+}
+function Zhengfa(avatarconfig,avatarpos,basedmg){
     var avatarpos = avatarpos - 1
     var avatarHealth = avatarconfig[avatarpos]["health"]
     var dmg = basedmg + 2
@@ -28,11 +37,45 @@ export function CauseDmg(avatarconfig,avatarpos,basedmg,dmgelement){
     var avatarpos = avatarpos - 1
     var avatarAttachedElement=avatarconfig[avatarpos]["attachedElement"]
     var avatarHealth = avatarconfig[avatarpos]["health"]
-    if (basedmg>avatarHealth){
+    /*if (basedmg>avatarHealth){
         console.log("角色死掉了")
         avatarconfig[avatarpos]["health"]=0
     } else {
-        if (avatarAttachedElement==1)
+        */if (avatarAttachedElement==1){
+            if (avatarAttachedElement!=null){
+                switch(avatarAttachedElement){
+                    case 3,5,6,7:
+                        console.log("扩散")
+                        switch(avatarpos){
+                            case 0:
+                                avatarconfig[avatarpos]["health"]=avatarHealth-basedmg
+                                if (avatarconfig[avatarpos]["health"]<=0){
+                                    console.log("角色死掉了")
+                                    avatarconfig[avatarpos]["health"]=0
+                                } else {
+                                    avatarconfig[avatarpos]["attachedElement"]=null
+                                }
+                                avatarconfig[avatarpos+1]["attachedElement"]=avatarAttachedElement
+                                avatarconfig[avatarpos+1]["health"]=avatarconfig[avatarpos+1]["health"]-1
+                                if (avatarconfig[avatarpos+1]["health"]<=0){
+                                    console.log("角色死掉了")
+                                    avatarconfig[avatarpos+1]["health"]=0
+                                } else {
+                                    avatarconfig[avatarpos]["attachedElement"]=null
+                                }
+                                avatarconfig[avatarpos+2]["attachedElement"]=avatarAttachedElement
+                                avatarconfig[avatarpos+2]["health"]=avatarconfig[avatarpos+2]["health"]-1
+                                if (avatarconfig[avatarpos+2]["health"]<=0){
+                                    console.log("角色死掉了")
+                                    avatarconfig[avatarpos+2]["health"]=0
+                                } else {
+                                    avatarconfig[avatarpos]["attachedElement"]=null
+                                }
+                        }
+
+                }
+            }
+        }
         if (avatarAttachedElement==5){
             if (dmgelement==6){
                 var dmg=basedmg+2
@@ -46,4 +89,3 @@ export function CauseDmg(avatarconfig,avatarpos,basedmg,dmgelement){
             }
         }
     }
-}
